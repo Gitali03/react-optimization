d3a2b2365663fde0d1621b1dc95f6a5d5695ded6
Gitali03 <aonermtk@gmail.com>
2025-05-14 13:04:24 +0300

    key

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,8 @@ function App() {
       <Header />
       <main>
         <ConfigureCounter onSet={handleSetCount}/>
-        <Counter initialCount={chosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
+        <Counter key={chosenCount} initialCount={0}/>
       </main>
     </>
   );
